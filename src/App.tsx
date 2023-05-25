30a8685f7c8532e62fc99635ac6bc225ed38105b
vkroyer <vebjorn.kroyer@gmail.com>
2023-05-26 00:26:57 +0200

    show and hide component excercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const alertChildren = (
+    <>
+      <p>My alert</p>
+      <button
+        type="button"
+        className="btn-close"
+        aria-label="Close"
+        onClick={() => setAlertVisibility(false)}
+      ></button>
+    </>
+  );
+
   return (
-    <Button color="info" onClick={() => console.log("Clicked")}>
-      My Button
-    </Button>
+    <>
+      {alertVisible && <Alert children={alertChildren}></Alert>}
+      <Button
+        color="info"
+        onClick={() => {
+          setAlertVisibility(true);
+        }}
+      >
+        My Button
+      </Button>
+    </>
   );
 }
 
