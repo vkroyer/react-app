0028a7cb15e520d26873be85f42c2191f0afbf96
vkroyer <vebjorn.kroyer@gmail.com>
2023-05-25 23:28:15 +0200

    alerts and passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Boston", "Oslo", "Beograd"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+    <div className="alert alert-primary">
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
