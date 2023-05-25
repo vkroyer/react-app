0fb02417617f66e1d41ca3b4e7a617d70d078f11
vkroyer <vebjorn.kroyer@gmail.com>
2023-05-25 23:11:05 +0200

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "Boston", "Oslo", "Beograd"];
+
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
