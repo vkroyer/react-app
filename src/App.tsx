33fc9da555939e8866732e30c5227a110eb66df1
vkroyer <vebjorn.kroyer@gmail.com>
2023-05-25 23:44:14 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="alert alert-primary">
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-    </div>
+    <Button color="info" onClick={() => console.log("Clicked")}>
+      My Button
+    </Button>
   );
 }
 
